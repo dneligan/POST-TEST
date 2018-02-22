b49f12067d553fe5bc9650541b01020c9e90078d
diarmuidn4 <diarmuidn4@gmail.com>
2018-02-22 11:28:39 +0000

    changes

--- test.php
+++ test.php
@@ -3,6 +3,6 @@ session_start();
 $_SESSION['name'] = $_POST['name'];
 $name = $_SESSION['name'];
 $_SESSION['email'] = $_POST['email'];
-$name = $_SESSION['email'];
+$email = $_SESSION['email'];
 echo $name, $email; 
 ?>
