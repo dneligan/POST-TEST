cfe80b9f76819c21a668f021a4d3f29549d306c4
diarmuidn4 <diarmuidn4@gmail.com>
2018-02-22 11:24:56 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<form method="POST" action="test.php">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+$_SESSION['name'] = $_POST['name'];
+$name = $_SESSION['name'];
+$_SESSION['email'] = $_POST['email'];
+$name = $_SESSION['email'];
+echo $name; 
+echo $email;
+?>
